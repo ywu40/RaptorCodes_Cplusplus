f5caa54ffc36ef2261a8c9daa8aa78655e96cf87
ywu40 <yeqingwu2011@gmail.com>
2015-04-19 18:36:55 -0700

    C++ codes for Raptor encoder/decoder on Windows

---
+++ RaptorCodes/inc/raptor_defines.h
@@ -0,0 +1,44 @@
+/*!
+*
+* \file   raptor_defines.h
+*
+* \brief  Header file of outputing error message for porting
+*
+*
+* \par    Include files
+*         -     
+*
+* \par    Copyright (c) 2011 Yeqing Wu
+*
+* This library is free software; you can redistribute it and/or modify it 
+* under the terms of the GNU Lesser General Public License as published by 
+* the Free Software Foundation. This software is developed by refering to the software wrote by Vicente Sirvent,
+* but many parts(especially some critical parts) of the raptor codes in that software have not been implemented 
+* and there are lots of bugs and logical errors in that software. So I re-design software and implement it.
+*
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+* See the GNU Lesser General Public License for more details.
+* You should have received a copy of the GNU Lesser General Public License along with this library; 
+* if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+* 
+* \version 
+* Revision of last commit: $Rev$ 
+* Author of last commit  : $Author$ 
+* Date of last commit    : $Date$ 
+*
+*/
+
+#ifndef __RAPTOR_DEFINES_H__
+#define __RAPTOR_DEFINES_H__
+
+#include <stdio.h>
+
+#define DBG printf
+#define ERR printf
+
+
+#endif /* __RAPTOR_DEFINES_H__ */
+
+/* end of raptor_defines.h */
